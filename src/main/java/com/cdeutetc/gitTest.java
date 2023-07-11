745f1148b3e96b30dd6c47ad6c52da74e9de4995
jhh <2236071412@qq.com>
2023-07-11 11:02:04 +0800

    fix-second

--- src/main/java/com/cdeutetc/gitTest.java
+++ src/main/java/com/cdeutetc/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("hot-fix1");
     }
 }
